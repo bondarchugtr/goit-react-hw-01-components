5228d0328f1c716579950536b0d71c367a099cc2
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-22 01:43:29 +0200

    add react

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import PropTypes, { number } from 'prop-types';
+
+Product.propTypes = {
+  username: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+  avatar: PropTypes.string,
+  stats: PropTypes.object,
+}
+
+
+
