20de5082f3798aec920a9cba9c2778075bd09e49
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-11-25 17:43:36 +0200

    Updates

---
+++ precache-manifest.bdf302a760303fa6da082ed7759fbf0b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d313ba4bbe074ff0f67b6d7a1153b3b",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "b978fded83aee97f7d6b",
+    "url": "/goit-react-hw-01-components/static/js/2.e062fd5c.chunk.js"
+  },
+  {
+    "revision": "4078df17e57140d0628a",
+    "url": "/goit-react-hw-01-components/static/js/main.80fe21f2.chunk.js"
+  },
+  {
+    "revision": "b0f8f2e436fc577230d9",
+    "url": "/goit-react-hw-01-components/static/js/runtime~main.681d89f7.js"
+  }
+]);
